aaefbfcd0f826857fd7630de869ff7e6b40be5f2
prussian <genunrest@gmail.com>
2016-09-03 14:04:57 -0400

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+For
+---
+
+OS X - Irssi
+
+Irssi is pretty much the only good irc client I know of for OS X
+
+Depends
+-------
+
+The Following binaries should be findable in your \$PATH.
+
+    terminal-notify
+
+Get with homebrew.
+    
+    brew install terminal-notify
+
+Warnings
+--------
+
+Due to invocation of system, it may be insecure.
+
+I tested it and I can't seem to trigger any arbitrary code execution, but I may not have tried hard enough.
+
+If you know of a safer way of doing this, send a pull request or drop an issue.
---
+++ mac-notify.pl
@@ -0,0 +1,56 @@
+# Copyright 2016 prussian <genunrest@gmail.com>>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# NOTE:
+# This program requires terminal-notifier
+# can be aquired via homebrew (brew install terminal-notifier)
+
+use strict;
+use Irssi;
+
+our $VERSION = '1.0.0';
+our %IRSSI = (
+	authors     => 'prussian',
+	contact     => 'genunrest@gmail.com',
+	name        => 'mac-notify',
+	description => 'Sends notifications using OS X specific terminal-notifier command. Due to invokation of system(), it may be dangerous',
+	license     => 'GPL v3+',
+);
+
+sub priv_msg {
+	my ($server, $msg, $nick, $address) = @_;
+
+	system("terminal-notifier", "-message", "'".$msg."'", "-title", "'".$nick."'", ">> /dev/null 2>&1 &");
+}
+
+sub hilight {
+	my ($dest, $text, $msg) = @_;
+	my $server = $dest->{server};
+
+	# Check if we should notify user of message:
+	# * if message is notice or highligh type
+	# * if the channel belongs to the current server
+	# * if the user is not focused on the channel window
+	if (!($server &&
+		  $dest->{level} & (MSGLEVEL_HILIGHT | MSGLEVEL_NOTICES) &&
+		  $server->ischannel($dest->{target}))) {
+		return;
+	}
+
+	my $network = $server->{tag};
+	system("terminal-notifier", "-message", "'".$msg."'", "-title", "'".$dest->{target}."'", ">> /dev/null 2>&1 &");
+}
+
+Irssi::signal_add_last('message private' => \&priv_msg);
+Irssi::signal_add_last('print text' => \&hilight);
